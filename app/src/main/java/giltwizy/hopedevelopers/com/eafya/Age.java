418580db905fa31bfe313061d0da22bd403ccc0d
Giltwizy <giltonbosco@yahoo.com>
2019-11-12 19:21:42 +0300

    designed age

--- app/src/main/java/giltwizy/hopedevelopers/com/eafya/Age.java
+++ app/src/main/java/giltwizy/hopedevelopers/com/eafya/Age.java
@@ -3,6 +3,9 @@ package giltwizy.hopedevelopers.com.eafya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class Age extends AppCompatActivity {
 
@@ -10,5 +13,9 @@ public class Age extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_age);
+
+        TextView miakakumi = findViewById(R.id.miakakumi);
+
+
     }
 }
